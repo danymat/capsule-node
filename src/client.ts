aba6aed3e04fb3d4a086d11be4c5c3206966e85d
danymat <d.danymat@gmail.com>
2021-12-30 13:56:58 +0100

    feat: Add axios

--- src/client.ts
+++ src/client.ts
@@ -1,2 +1,6 @@
-// import { io } from "socket.io-client";
+import { io } from "socket.io-client";
+import axios from "axios";
 
+const socket = io("http://localhost")
+
+socket.on("connection", () => { console.log("connected") })
